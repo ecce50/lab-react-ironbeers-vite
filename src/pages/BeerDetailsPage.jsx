f9af9c39c82a75468ab552a07085266d5587a017
Peter R <prodger2@gmail.com>
2023-08-01 08:39:04 +0200

    Beer details page – doesn't quite work, but I can't work out why

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,3 +1,42 @@
-function BeerDetailsPage() {}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
+function BeerDetailsPage() {
+  const { beerId } = useParams();
+
+  const [theBeer, setTheBeer] = useState("");
+
+  const getTheBeer = async () => {
+    try {
+      const result = await axios.get(
+        `https://ih-beers-api2.herokuapp.com/beers/${beerId}`
+      );
+      console.log("The beer data: ", result.data);
+      setTheBeer(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTheBeer();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <img src={theBeer.image_url} />
+      </div>
+      <div>
+        <h1>{theBeer.name};</h1>
+        <h2>{theBeer.tagline};</h2>
+        <p>{theBeer.first_brewed};</p>
+        <p>{theBeer.attenuation_level};</p>
+        <p>{theBeer.description};</p>
+        <p>{theBeer.contributed_by};</p>
+      </div>
+    </>
+  );
+}
 export default BeerDetailsPage;
