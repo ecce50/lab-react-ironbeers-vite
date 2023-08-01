be231414734f7d90347cdf1a003826548714e310
Peter R <prodger2@gmail.com>
2023-08-01 08:07:22 +0200

    updated css

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import "./AllBeersPage.css";
 
 function AllBeersPage() {
   const [allBeers, setAllBeers] = useState([]);
@@ -24,13 +25,23 @@ function AllBeersPage() {
   return (
     <>
       {allBeers.map((aBeer) => (
-        <Link to={`/BeerDetailsPage/${aBeer._id}`} key={aBeer._id}>
-          <p>{aBeer.name}</p>
-          <p>{aBeer.contributed_by}</p>
-          <p>{aBeer.tagline}</p>
-        </Link>
+        <div id="beerInfo" key={aBeer._id}>
+          <img src={aBeer.image_url} alt={aBeer.name} />
+          <div id="beerText">
+            <Link to={`/BeerDetailsPage/${aBeer._id}`}>
+              <h3>{aBeer.name}</h3>
+            </Link>
+            <p>{aBeer.tagline}</p>
+            <p>Contributed by: {aBeer.contributed_by}</p>
+          </div>
+        </div>
       ))}
     </>
   );
 }
 export default AllBeersPage;
+//  <Link to={`/BeerDetailsPage/${aBeer._id}`} key={aBeer._id}>
+//    <p>{aBeer.name}</p>
+//    <p>{aBeer.contributed_by}</p>
+//    <p>{aBeer.tagline}</p>
+//  </Link>;
